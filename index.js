b821508cef26fd598744c9413cb240dc0d146086
Md-Saifullah <rssaiful67@gmail.com>
2022-05-07 20:53:04 +0600

    4th commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var result = $("#textFild");
+
 function insertNumbers(number) {
 	var existingNumber = $("#textFild").val();
 	$("#textFild").val(existingNumber + number);
@@ -11,3 +13,22 @@ function finalResult() {
 	var calculatedResult = eval($("#textFild").val());
 	$("#textFild").val(calculatedResult);
 }
+
+
+function deleteNumber(){
+	var present = result.val()
+    if(present!=''){
+		result.val(present.slice(0, -1))
+	}
+    
+}
+
+
+
+
+
+// var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(number);
+// var result = number.slice(0, -1);
+// console.log(result);
+
